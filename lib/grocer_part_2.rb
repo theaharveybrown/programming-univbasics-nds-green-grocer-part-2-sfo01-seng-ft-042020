db138bc0e800669ff671bd0d920b13f2977ccfb7
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-04-14 15:50:24 +0000

    Done.

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -51,15 +51,7 @@ def checkout(cart, coupons)
     price = item[:price] * item[:count]
     total += price
   end 
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+
   if total >= 100 
     total = 0.9 * total
   end
@@ -67,19 +59,3 @@ def checkout(cart, coupons)
   total
 end
 
-coupon = [
-  {:item => "AVOCADO", :num => 2, :cost => 5.00}
-]
-
-cart = [
-  {:item => "PEANUT BUTTER", :price => 3.00, :clearance => true,  :count => 2},
-  {:item => "KALE", :price => 3.00, :clearance => false, :count => 3},
-  {:item => "SOY MILK", :price => 4.50, :clearance => true,  :count => 1}
-]
-unconsolidated_cart = [
-  {:item => "AVOCADO", :price => 3.00, :clearance => true },
-  {:item => "AVOCADO", :price => 3.00, :clearance => true },
-  {:item => "KALE", :price => 3.00, :clearance => false}
-]
-
-checkout(unconsolidated_cart, coupon)
